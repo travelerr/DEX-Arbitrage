155e827ef9f11bd28d196e57b662c02e254a49c6
Justin <foliofinance@gmail.com>
2023-08-16 19:47:41 -0400

    inital commit

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  networks: {
+    aurora: {
+      url: `https://mainnet.aurora.dev`,
+      accounts: [process.env.privateKey],
+    },
+    auroraTestnet: {
+      url: `https://testnet.aurora.dev`,
+      accounts: [process.env.privateKey],
+    },
+    fantom: {
+      url: `https://rpc.ftm.tools/`,
+      accounts: [process.env.privateKey],
+    },
+    sepolia: {
+      url: `https://rpc.sepolia.org`,
+      accounts: [process.env.privateKey],
+    },
+  },
+  solidity: {
+    compilers: [
+      { version: "0.8.7" },
+      { version: "0.7.6" },
+      { version: "0.6.6" },
+    ],
+  },
+};
